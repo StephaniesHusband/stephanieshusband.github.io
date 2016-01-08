b9152119edc6c9a7c9daa613a0180ffe87831498
StephaniesHusband@gmail.com <StephaniesHusband@gmail.com>
2016-01-08 15:44:06 -0600

    use bower-concat. ftp the fonts dir.

--- route/Gruntfile.js
+++ route/Gruntfile.js
@@ -1,32 +1,55 @@
 /*global module:false*/
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    "ftp-deploy": {
-      build: {
-        auth: {
-          host: 'erwin5.com',
-          port: 21,
-          authKey: 'fedex.erwin5'
-        },
-        src: 'src',
-        dest: '/route',
-        exclusions: ['.*.swp']
-      }
-    },
+   // Project configuration.
+   grunt.initConfig({
 
-    wiredep: {
-       task: {
-          src: "src/**/*.html"
-       }
-    }
-  });
+      "ftp-deploy": {
+         src: {
+            auth: {
+               host: "erwin5.com",
+               port: 21,
+               authKey: "fedex.erwin5"
+            },
+            src: [
+               "src"
+            ],
+            dest: "/route",
+            exclusions: [".*.swp"]
+         },
+         fonts: {
+            auth: {
+               host: "erwin5.com",
+               port: 21,
+               authKey: "fedex.erwin5"
+            },
+            src: [
+               "bower_components/font-awesome/fonts"
+            ],
+            dest: "/fonts"
+         }
+      },
+
+      "bower_concat": {
+         all: {
+            dest: "src/_bower.js",
+            cssDest: "src/_bower.css",
+            mainFiles: (function() {
+               var out = {};
+               var overrides = grunt.file.readJSON("./bower.json").overrides;
+
+               Object.keys(overrides).forEach(function(key) {
+                  out[key] = overrides[key].main;
+               });
+
+               return out;
+            }())
+         }
+      }
+   });
 
   grunt.loadNpmTasks("grunt-ftp-deploy");
-  grunt.loadNpmTasks("grunt-wiredep");
+  grunt.loadNpmTasks("grunt-bower-concat");
 
-  grunt.registerTask('deploy', ['ftp-deploy']);
-
-  grunt.registerTask("default", ["wiredep"]);
+  grunt.registerTask("deploy", ["ftp-deploy"]);
 };
